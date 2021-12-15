549e119d9bdea9cd89cb486e1fec27a8e5c5cbab
Thomas Cardon <thomas.cardon35@gmail.com>
2021-12-15 16:46:09 +0100

    3 routing, redirect, wildcard, notfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAccueilComponent } from './pages/page-accueil/page-accueil.component';
+import { PageDetailsComponent } from './pages/page-details/page-details.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'home', pathMatch: 'full'}, // pathMatch : 'full' --> vérifie si toute l'url correspond
+  {path: 'home', component: PageAccueilComponent},
+  {path: 'details', component: PageDetailsComponent},
+  {path: '**', component: PageNotFoundComponent} // '**' a mettre en dernier
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
