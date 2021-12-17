cd1de446f2014e6ae1156ee8dbd589fd6c7b8ba9
Thomas Cardon <thomas.cardon35@gmail.com>
2021-12-17 14:20:42 +0100

    8.bonus - avis component

--- src/app/components/avis-bar/avis-bar.component.ts
+++ src/app/components/avis-bar/avis-bar.component.ts
@@ -6,28 +6,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avis-bar.component.scss']
 })
 export class AvisBarComponent implements OnInit {
-  starState: string[];
+  starStates: {stateSelectedUser : boolean, stateHoverUser : boolean}[];
+
   constructor() {
-    this.starState = ['star', 'star', 'star', 'star', 'star'];
+    this.starStates = [];
+
+    for (let index = 0; index < 5; index++) {
+      this.starStates.push(
+        {
+          stateSelectedUser : false,
+          stateHoverUser : false
+        }
+      );
+    }
    }
 
   ngOnInit(): void {
   }
 
   onMouseOver(index: number) {
-    //console.log("star over", index);
-    for (let i = 0; i < this.starState.length ; i++) {
+    console.log("star over", index);
+    for (let i = 0; i < this.starStates.length ; i++) {
       if(i <= index) {
-        this.starState[i] = 'star-fill';
+        this.starStates[i].stateHoverUser = true;
       } else {
-        this.starState[i] = 'star';
+        this.starStates[i].stateHoverUser = false;
       }
     }
   }
 
   onMouseLeave() {
     // this.starState = ['star', 'star', 'star', 'star', 'star'];
-    this.starState = this.starState.map(x => x = 'star');
+    const tempTab = [];
+    for (let index = 0; index < this.starStates.length; index++) {
+      tempTab.push(
+        {
+          stateSelectedUser : this.starStates[index].stateSelectedUser,
+          stateHoverUser : this.starStates[index].stateSelectedUser
+        }
+      );
+    }
+    this.starStates = [...tempTab];
+  }
+
+  onClickStar(starIndex: number) {
+    for (let i = 0; i < this.starStates.length ; i++) {
+      if(i <= starIndex) {
+        this.starStates[i].stateSelectedUser = true;
+      } else {
+        this.starStates[i].stateSelectedUser = false;
+      }
+    }
   }
 
 }
