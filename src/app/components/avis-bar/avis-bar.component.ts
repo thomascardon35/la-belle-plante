7d7623eeb2a4cc7fa69ea93c64c9443b7f211883
Thomas Cardon <thomas.cardon35@gmail.com>
2021-12-17 14:14:34 +0100

    8 - icon start + component (etoiles débuggées)

--- src/app/components/avis-bar/avis-bar.component.ts
+++ src/app/components/avis-bar/avis-bar.component.ts
@@ -6,31 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avis-bar.component.scss']
 })
 export class AvisBarComponent implements OnInit {
-
   starState: string[];
-
   constructor() {
-    this.starState = ["star","star","star","star","star"];
+    this.starState = ['star', 'star', 'star', 'star', 'star'];
    }
 
   ngOnInit(): void {
   }
 
-  onMouseOver(index : number){
-    console.log("star over", index);
-    for (let i = 0; i <= index; i++) {
-      if(i<=index){
-      this.starState[i]= 'star-fill';
-      }else{
-        this.starState[i]= 'star';
+  onMouseOver(index: number) {
+    //console.log("star over", index);
+    for (let i = 0; i < this.starState.length ; i++) {
+      if(i <= index) {
+        this.starState[i] = 'star-fill';
+      } else {
+        this.starState[i] = 'star';
       }
     }
   }
 
-  onMouseLeave(){
-    //this.starState = ["star","star","star","star","star"];
-    this.starState = this.starState.map(x=> x = 'star');
-
+  onMouseLeave() {
+    // this.starState = ['star', 'star', 'star', 'star', 'star'];
+    this.starState = this.starState.map(x => x = 'star');
   }
 
 }
